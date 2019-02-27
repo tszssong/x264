28a312fa07829674f42cb94e96d75387b39f7ac2
zheng.meisong <zheng.meisong@immomo.com>
2019-02-27 15:13:52 +0800

    修改脚本目录参数

--- ratio.sh
+++ ratio.sh
@@ -1,11 +1,12 @@
-mkdir /Volumes/song/videoQ_data/xiaopeng/"roi"$1"_"$2
-for line in `cat /Volumes/song/videoQ_data/xiaopeng/$1.txt`
+dir="/Volumes/song/videoQ_data/3/"
+mkdir $dir"roi"$1"_"$2
+for line in `cat $dir/$1.txt`
 do
 flvname=${line}".flv"
 txtname=${line}".txt"
-bitrate=`ffprobe -show_format  /Volumes/song/videoQ_data/xiaopeng/$1/$flvname | grep "bit_rate" `
+bitrate=`ffprobe -show_format  $dir/$1/$flvname | grep "bit_rate" `
 echo $bitrate
 val=`expr $[bitrate] \* $[$2] / 10000`
 echo $val
-./x264 --bitrate $val --bframe 0 --keyint 30 --roifile /Volumes/song/videoQ_data/xiaopeng/$1"ROI"/$txtname  -o /Volumes/song/videoQ_data/xiaopeng/"roi"$1"_"$2/$flvname /Volumes/song/videoQ_data/xiaopeng/$1/$flvname
+./x264 --bitrate $val --bframe 0 --keyint 30 --roifile $dir$1"ROI"/$txtname  -o $dir"roi"$1"_"$2/$flvname $dir$1/$flvname
 done
