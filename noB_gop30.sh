897a0d3fbcbd7b04d73a316007eea8e6daab30f8
zheng.meisong <zheng.meisong@immomo.com>
2019-03-25 14:35:50 +0800

    clear sh

--- noB_gop30.sh
+++
@@ -1,7 +0,0 @@
-mkdir /Volumes/song/videoQ_data/xiaopeng/"roi"$1
-for line in `cat /Volumes/song/videoQ_data/xiaopeng/5face.txt`
-do
-flvname=${line}".flv"
-txtname=${line}".txt"
-./x264 --bitrate $1 --bframe 0 --keyint 30 --roifile /Volumes/song/videoQ_data/xiaopeng/5faceROI/$txtname  -o /Volumes/song/videoQ_data/xiaopeng/"roi"$1/$flvname /Volumes/song/videoQ_data/xiaopeng/5face/$flvname
-done
